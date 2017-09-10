f8facd89638ce214fc8aa72e0ee9b4f04c2609da
Maurizio Bonani <maurizio.bonani@vecomp.it>
2017-09-10 20:34:26 +0200

    Added watchers to x,y,w,h

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
     <div style="height: 500px; width: 500px; margin: 20px; border: 1px solid red; position: relative;">
-      <VueDraggableResizable :x="50" :y="50" :w="400" :h="400" :parent="true">
+      <vue-draggable-resizable :x="x" :y="y" :w="w" :h="h" :parent="true" @resizing="onResize" @dragging="onDrag">
         <p>Component</p>
       </VueDraggableResizable>
+    </div>
+    <div>
+      X <input type="number" v-model.number.lazy="x"> / Y <input type="number" v-model.number="y"> <br>
+      Width <input type="number" v-model.number.lazy="w"> / Height <input type="number" v-model.number="h">
     </div>
   </div>
 </template>
@@ -14,13 +18,33 @@ import VueDraggableResizable from './components/vue-draggable-resizable'
 export default {
   name: 'app',
   components: {
-    VueDraggableResizable
+    'vue-draggable-resizable': VueDraggableResizable
+  },
+  data: function () {
+    return {
+      x: 50,
+      y: 50,
+      w: 200,
+      h: 200
+    }
+  },
+  methods: {
+    onResize: function (left, top, width, height) {
+      this.x = left
+      this.y = top
+      this.w = width
+      this.h = height
+    },
+    onDrag: function (left, top) {
+      this.x = left
+      this.y = top
+    }
   }
 }
 </script>
 
 <style>
-  .active {
+  .vdr {
     border: 1px dashed black;
   }
 </style>
