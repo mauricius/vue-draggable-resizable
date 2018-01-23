7e8c3a0fa44b8163cd58fcfa3785a2dad4214585
Maurizio Bonani <maurizio.bonani@vecomp.it>
2018-01-23 23:22:10 +0100

    Fix webpack umd definition

--- build/webpack.dist.js
+++ build/webpack.dist.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: '[name].js',
-    library: 'vue-draggable-resizable',
+    library: 'VueDraggableResizable',
     libraryTarget: 'umd'
   },
   devtool: false,
