c71113ab8b8cd45fb68e469b77fbc57db0590b20
Andrzej Ojczenasz <codelaborer@gmail.com>
2018-03-30 22:53:06 +0200

    not resizeable to demo added

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,14 @@
         <p>Component</p>
       </VueDraggableResizable>
     </div>
+    <div style="height: 500px; width: 500px; margin: 20px; border: 1px solid red; position: relative;">
+      <VueDraggableResizable :x="50" :y="50" :w="150" :h="50" :parent="true" :resizable="false" :dragHandle="'.dragHandle'">
+        <p>Component</p>
+        <span class="dragHandle">
+          =
+        </span>
+      </VueDraggableResizable>
+    </div>
   </div>
 </template>
 
@@ -22,5 +30,13 @@ export default {
 <style>
   .active {
     border: 1px dashed black;
+  }
+  .dragHandle {
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 5px;
+    font-size: 20px;
+    background-color: lightgrey;
   }
 </style>
