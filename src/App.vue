326475578859220ce5393e57bc3a4c76b6c988ed
Andrzej Ojczenasz <codelaborer@gmail.com>
2018-04-04 11:12:24 +0200

    4 cases of use in demo app

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,23 @@
       </VueDraggableResizable>
     </div>
     <div style="height: 500px; width: 500px; margin: 20px; border: 1px solid red; position: relative;">
+      <h2>Grid 30x30</h2>
+      <VueDraggableResizable :x="50" :y="50" :w="400" :h="400" :grid="[30, 30]" :parent="true">
+      <p>Component</p>
+      </VueDraggableResizable>
+    </div>
+    <div style="height: 500px; width: 500px; margin: 20px; border: 1px solid red; position: relative;">
+      <h2>Custom handler</h2>
       <VueDraggableResizable :x="50" :y="50" :w="150" :h="50" :parent="true" :resizable="false" :dragHandle="'.dragHandle'">
+        <p>Component</p>
+        <span class="dragHandle">
+          =
+        </span>
+      </VueDraggableResizable>
+    </div>
+    <div style="height: 500px; width: 500px; margin: 20px; border: 1px solid red; position: relative;">
+      <h2>Grid 30x30 and handler</h2>
+      <VueDraggableResizable :x="50" :y="50" :w="150" :h="50" :parent="true" :grid="[30, 30]" :resizable="false" :dragHandle="'.dragHandle'">
         <p>Component</p>
         <span class="dragHandle">
           =
@@ -31,6 +47,11 @@ export default {
   .active {
     border: 1px dashed black;
   }
+  h2 {
+    opacity: 0.3;
+    padding: 0 0 0 20px;
+    z-index: -1;
+  }
   .dragHandle {
     position: absolute;
     top: 0;
@@ -38,5 +59,8 @@ export default {
     padding: 5px;
     font-size: 20px;
     background-color: lightgrey;
+  }
+  .vdr {
+    background: white;
   }
 </style>
