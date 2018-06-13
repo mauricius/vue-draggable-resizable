43f87bd5eaf4be400e267b1b17979956f0bfe910
guoyangyang <guoyangyang@b.360.cn>
2018-06-13 16:21:31 +0800

    make x,y,w,h reactive

--- src/utils/fns.js
+++ src/utils/fns.js
@@ -1,3 +1,8 @@
 export function isFunction (func) {
   return typeof func === 'function' || Object.prototype.toString.call(func) === '[object Function]'
 }
+
+export function isNumber(val) {
+  return typeof val === 'number'
+}
+
