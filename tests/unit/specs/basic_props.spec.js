158a97fa3b44b74a7ce8f37919ec7b42baab5215
Maurizio Bonani <maurizio.bonani@gmail.com>
2018-12-26 14:01:03 +0100

    Add basic tests to maxWidth and maxHeight

--- tests/unit/specs/basic_props.spec.js
+++ tests/unit/specs/basic_props.spec.js
@@ -595,5 +595,73 @@ describe('basic props', function () {
     })
   })
 
+  /********************************
+   * maxWidth and maxHeight props *
+   ********************************/
+
+  describe('maxWidth and maxHeight props', function () {
+    it('should pass maxWidth and maxHeight as props', function () {
+      wrapper = mount(VueDraggableResizable, {
+        propsData: {
+          maxWidth: 200,
+          maxHeight: 300
+        }
+      })
+
+      expect(wrapper.props().maxWidth).to.equal(200)
+      expect(wrapper.props().maxHeight).to.equal(300)
+    })
+
+    it('should react to maxWidth and maxHeight prop changes', function () {
+      wrapper = mount(VueDraggableResizable, {
+        attachToDocument: true,
+        propsData: {
+          maxWidth: 200,
+          maxHeight: 300
+        }
+      })
+
+      wrapper.setProps({ maxWidth: 300, maxHeight: 200 })
+
+      expect(wrapper.props().maxWidth).to.equal(300)
+      expect(wrapper.props().maxHeight).to.equal(200)
+    })
+
+    it('should not resize the component over maxWidth and maxHeight', function (done) {
+      wrapper = mount(VueDraggableResizable, {
+        attachToDocument: true,
+        propsData: {
+          maxWidth: 100,
+          maxHeight: 100,
+          w: 100,
+          h: 100,
+          active: true
+        }
+      })
+
+      wrapper.vm.$nextTick(() => {
+        const $el = wrapper.vm.$el
+
+        const rect = $el.querySelector('div.handle-br').getBoundingClientRect()
+        const fromX = rect.left
+        const fromY = rect.top
+
+        syn.drag(
+          $el.querySelector('div.handle-br'),
+          {
+            from: { pageX: fromX, pageY: fromY },
+            to: { pageX: fromX + 50, pageY: fromY + 50 }
+          },
+          function () {
+            expect($el.style.width).to.equal('100px')
+            expect($el.style.height).to.equal('100px')
+
+            done()
+          }
+        )
+      })
+    })
+  })
+
   afterEach(() => wrapper.destroy())
 })
