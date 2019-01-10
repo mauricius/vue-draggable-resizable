639f52d95dd361cb0792560b2ea92ee4b35b4063
Maurizio Bonani <maurizio.bonani@gmail.com>
2019-01-10 23:22:50 +0100

    Add wrapper for auto-install component via script tag

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './components/vue-draggable-resizable.css'
 
-export { default } from './components/vue-draggable-resizable'
+import VueDraggableResizable from './components/vue-draggable-resizable'
+
+export function install (Vue) {
+  if (install.installed) return
+  install.installed = true
+  Vue.component('VueDraggableResizable', VueDraggableResizable)
+}
+
+const plugin = {
+  install
+}
+
+let GlobalVue = null
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
+
+export default VueDraggableResizable
