352f3a230f2e9873b4478d38e84a6db574ce375f
mauricius <maurizio.bonani@gmail.com>
2019-12-02 22:07:26 +0100

    publish: Add scale prop
    generated from commit 5476c2ee1d4b97a1e42dc1df4833e6ebfdf22b26

--- vendors~main.19ef01e327ffc4d145ee.bundle.js
+++ vendors~main.19ef01e327ffc4d145ee.bundle.js
@@ -44,4 +44,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.ae552339176fd24bd03b.bundle.js.map
+//# sourceMappingURL=vendors~main.19ef01e327ffc4d145ee.bundle.js.map
