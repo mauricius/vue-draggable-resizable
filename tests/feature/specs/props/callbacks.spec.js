f910e8314a29b3f9803370a71582d3c060643c97
Maurizio Bonani <maurizio.bonani@gmail.com>
2020-04-11 19:15:41 +0200

    More tests

--- tests/feature/specs/props/callbacks.spec.js
+++ tests/feature/specs/props/callbacks.spec.js
@@ -21,6 +21,22 @@ describe('`onDragStart` and `onResizeStart` props', function () {
     sinon.assert.called(onDragStartCallback)
   })
 
+  it('should prevent activation of the component if the `onDragStart` callback returns false', function () {
+    const onDragStartCallback = () => false
+
+    wrapper = mount(VueDraggableResizable, {
+      attachToDocument: true,
+      propsData: {
+        onDragStart: onDragStartCallback
+      }
+    })
+
+    wrapper.trigger('mousedown')
+
+    expect(wrapper.emitted()).to.not.have.property('activated')
+    expect(wrapper.classes()).to.not.contain('active')
+  })
+
   it('should call `onResizeStart` callback when the component is resized', function (done) {
     const onResizeStartCallback = sinon.spy()
 
@@ -48,6 +64,43 @@ describe('`onDragStart` and `onResizeStart` props', function () {
         },
         function () {
           sinon.assert.called(onResizeStartCallback)
+
+          done()
+        }
+      )
+    })
+  })
+
+  it('should prevent resizing the component if the `onResizeStart` callback returns false', function (done) {
+    const onResizeStartCallback = () => false
+
+    wrapper = mount(VueDraggableResizable, {
+      attachToDocument: true,
+      propsData: {
+        active: true,
+        w: 100,
+        h: 100,
+        onResizeStart: onResizeStartCallback
+      }
+    })
+
+    wrapper.vm.$nextTick(() => {
+      const $el = wrapper.vm.$el
+
+      const rect = $el.querySelector('div.handle-br').getBoundingClientRect()
+      const fromX = rect.left
+      const fromY = rect.top
+
+      syn.drag(
+        $el.querySelector('div.handle-br'),
+        {
+          from: { pageX: fromX, pageY: fromY },
+          to: { pageX: fromX + 50, pageY: fromY + 50 },
+          duration: 10
+        },
+        function () {
+          expect($el.style.width).to.equal('100px')
+          expect($el.style.height).to.equal('100px')
 
           done()
         }
--- tests/feature/specs/props/lock-aspect-ratio.spec.js
+++ tests/feature/specs/props/lock-aspect-ratio.spec.js
@@ -50,5 +50,44 @@ describe('`lock-aspect-ratio` prop', function () {
     })
   })
 
+  it('should not resize the component outside the parent node if `parent` prop is true and `lock-aspect-ratio` is set', function (done) {
+    const ParentComponent = {
+      template: `<div class="parent" style="width: 500px; height: 500px;">
+        <vue-draggable-resizable :w="300" :h="400" :parent="true" :active="true" :lock-aspect-ratio="true"></vue-draggable-resizable>
+      </div>`,
+      components: {
+        VueDraggableResizable
+      }
+    }
+
+    wrapper = mount(ParentComponent, {
+      attachToDocument: true
+    })
+
+    wrapper.vm.$nextTick(() => {
+      const $el = wrapper.vm.$children[0].$el
+
+      const rect = $el.querySelector('div.handle-mr').getBoundingClientRect()
+      const fromX = rect.left
+      const fromY = rect.top
+
+      syn.drag(
+        $el.querySelector('div.handle-br'),
+        {
+          from: { pageX: fromX, pageY: fromY },
+          to: { pageX: fromX + 100, pageY: fromY },
+          duration: 10
+        },
+        function () {
+          expect($el.style.transform).to.equal('translate(0px, 0px)')
+          expect($el.style.width).to.equal('375px')
+          expect($el.style.height).to.equal('500px')
+
+          done()
+        }
+      )
+    })
+  })
+
   afterEach(() => wrapper.destroy())
 })
