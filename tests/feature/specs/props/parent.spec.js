f5365ea55385b0e64abd69e5cfd34e36413d964b
mauricius <maurizio.bonani@gmail.com>
2020-10-12 20:28:26 +0200

    Add test

--- tests/feature/specs/props/parent.spec.js
+++ tests/feature/specs/props/parent.spec.js
@@ -157,5 +157,60 @@ describe('`parent` prop', function () {
     })
   })
 
+  it('should resize correctly after the parent changes size to bigger values', function (done) {
+    const ParentComponent = {
+      template: `<div class="parent" style="width: 250px; height: 250px;">
+        <vue-draggable-resizable :w="200" :h="200" :parent="true" :active="true"></vue-draggable-resizable>
+      </div>`,
+      components: {
+        VueDraggableResizable
+      }
+    }
+
+    wrapper = mount(ParentComponent, {
+      attachToDocument: true
+    })
+
+    wrapper.vm.$nextTick(() => {
+      const $parent = wrapper.vm.$el
+      const $el = wrapper.vm.$children[0].$el
+
+      const rect = $el.querySelector('div.handle-br').getBoundingClientRect()
+      const fromX = rect.left
+      const fromY = rect.top
+
+      syn.drag(
+        $el.querySelector('div.handle-br'),
+        {
+          from: { pageX: fromX, pageY: fromY },
+          to: { pageX: fromX + 50, pageY: fromY + 50 },
+          duration: 10
+        },
+        function () {
+          $parent.style.width = '300px'
+          $parent.style.height = '300px'
+
+          window.dispatchEvent(new Event('resize'))
+
+          syn.drag(
+            $el.querySelector('div.handle-br'),
+            {
+              from: { pageX: fromX + 50, pageY: fromY + 50 },
+              to: { pageX: fromX + 100, pageY: fromY + 100 },
+              duration: 10
+            },
+            function () {
+              expect($el.style.transform).to.equal('translate(0px, 0px)')
+              expect($el.style.width).to.equal('300px')
+              expect($el.style.height).to.equal('300px')
+
+              done()
+            }
+          )
+        }
+      )
+    })
+  })
+
   afterEach(() => wrapper.destroy())
 })
